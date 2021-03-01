f1d037ac1659367b771381b0e4b6aa54cce956a9
Nelson Vilte <nelsonvilte@gmail.com>
2021-03-01 16:17:10 -0300

    Se configuraron arrow functions handleChange y handleSubmit en formulario Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Search = () => {
-    return (
-        <form
-        className='d-flex'
-        style={{ maxWidth: '25em', margin: '16px auto' }}
-      >
-        <input
-          className='form-control me-2'
-          type='search'
-          placeholder='Buscar por'
-          name='word'
-        />
-        <button className='btn btn-outline-success' type='submit'>
-          Buscar
-        </button>
-      </form>
-  
-    );
+  const handleChange = (e) => {
+    console.log("mira el evento");
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="d-flex"
+      style={{ maxWidth: "25em", margin: "16px auto" }}
+    >
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Buscar por"
+        name="word"
+        onChange={handleChange}
+      />
+      <button className="btn btn-outline-success" type="submit">
+        Buscar
+      </button>
+    </form>
+  );
 };
 
 export default Search;
