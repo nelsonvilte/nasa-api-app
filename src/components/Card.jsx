3c95393388ff7b86ecd448b13c12aa0d5b619c9d
nelsonvilte <nelsonvilte@gmail.com>
2026-06-06 03:59:48 -0300

    actualizar estilos de imagenes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,17 @@ const Card = ({ info }) => {
 
   return (
     <div className="col mb-3">
-      <div className="card" style={{ height: "auto" }}>
+      <div className="card" >
         <img
           src={info.links[0].href}
           className="card-img-top"
           alt="..."
-          style={{ width: "100%", height: "40vh" }}
+          style={{ width: "100%", height: "15rem", objectFit:"cover" }}
         />
         <div className="card-body">
           <h5 className="card-title">{info.data[0].title}</h5>
 
-          {infodata && <p className="card-text">{info.data[0].description}</p>}
+      {infodata && <p className="card-text">{info.data[0].description}</p>}
           <button
             onClick={() => setInfodata(!infodata)}
             className="btn btn-primary"
@@ -23,9 +23,9 @@ const Card = ({ info }) => {
             {infodata ? "Cerrar" : "Leer más"}
           </button>
         </div>
-        <div className="card-footer">
+         <div className="card-footer">
           <small className="text-muted">{info.data[0].date_created}</small>
-        </div>
+        </div> 
       </div>
     </div>
   );
